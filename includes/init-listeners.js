e75da4d4313be6a4920aa4397d0a1f755ac41530
PikachuEXE <pikachuexe@gmail.com>
2013-07-28 15:22:38 +0800

    * Keep re-running initialization code until no error

--- includes/init-listeners.js
+++ includes/init-listeners.js
@@ -3,9 +3,15 @@ var postInitListeners = [];
 // Some listeners need the window to get gBrowser, tabContainer
 function runPostInitListeners(window) {
   window.setTimeout(function () {
-    // Run module specific late initialisation code (after all init* listeners, and after most extensions):
-    for each (var listener in postInitListeners) {
-      listener(window);
+    try {
+      // Run module specific late initialisation code (after all init* listeners, and after most extensions):
+      for each (var listener in postInitListeners) {
+        listener(window);
+      }
     }
-  }, 0);
+    catch (ex) {
+      // Run again if something is not ready yet
+      runPostInitListeners(window);
+    }
+  }, 100);
 }
